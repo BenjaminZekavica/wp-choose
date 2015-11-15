52cb0db199caceec1218194a1391ca450296b62b
Benjamin Zekavica <info@benjamin-zekavica.de>
2015-11-15 11:38:11 +0100

    Create content.php

---
+++ content.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Content
+ */
+?>
+<section class="main">
+	<article class="post" id="post-<?php the_ID(); ?>">
+		<section class="post-content">
+        	<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+        	<p><?php echo the_content(); ?></p>
+    	</section>
+    		<aside class="meta">
+    			<p><time datetime="<?php echo get_the_time(Y-m-d-g-i-s); ?>"><?php echo get_the_date(); ?></time></p> 
+    			<p>Kategorie:<?php the_category(',') ?></p> 
+    			<p>Schlagworte <?php the_tags(""); ?></p>
+    			<p><a href="#comments"><?php comments_number( 'Kommentieren', '1 Kommentar', '% Kommentare' ); ?></a></p>
+    		</aside>
+    
+<footer>Kommentare</footer>
+    
+	</article><!-- /.post-->
+</section><!-- /.main-->
